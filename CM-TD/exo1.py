2f8373698b00072f20d059b8ed365be8a40c474b
Florent LORNE <florent.lorne@univ-smb.fr>
2023-09-21 15:27:49 +0200

    Adding choices to parser.add_argument

--- CM-TD/exo1.py
+++ CM-TD/exo1.py
@@ -12,15 +12,15 @@ def check():
     logging.debug("Check not finished yet. Don't use")
 
 if __name__=="__main__":
+
     logging.basicConfig(level=logging.DEBUG)
     parser=argparse.ArgumentParser(prog="exo1", description="Programme de test et d'initialisation de l'exo1 lié au CM et TD")
-    parser.add_argument("action", required=True,help="2 possible actions : check or init")
+    parser.add_argument("action", choices=["init","check"], help="2 possible actions : check or init")
     args=parser.parse_args()
     action=vars(args)['action']
     if action=="init":
         init()
     elif action=="check":
         check()
-    else:
-        print("Argument must be init or check")
+
     
