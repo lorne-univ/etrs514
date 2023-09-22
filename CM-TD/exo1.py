9ec8e7e1020541d7810dea7c939c98c3f27afb7f
Florent LORNE <florent.lorne@univ-smb.fr>
2023-09-22 10:23:45 +0200

    Add compare_two_text(text1, text2)

--- CM-TD/exo1.py
+++ CM-TD/exo1.py
@@ -6,6 +6,8 @@ import logging
 import subprocess
 import signal
 import shutil
+import difflib
+from pprint import pprint
 
 colors = {"red": "\033[31m", "green": "\033[32m", "reset": "\033[0m"}
 
@@ -37,14 +39,42 @@ def init():
         os.remove(path)
 
 
+def compare_two_text(text1, text2):
+    """
+    Give two text and compare them
+    """
+    text1 = text1.splitlines(keepends=False)
+    text2 = text2.splitlines(keepends=False)
+    differ = difflib.Differ()
+    diff = list(differ.compare(text1, text2))
+    same_text = True
+    for line in diff:
+        if line[0] != " ":
+            same_text = False
+            pprint(diff)
+            break
+    return same_text
+
+
 def check_content_of_file(file_path, expected_content):
-    with open(file_path, "r") as file:
-        content = file.read()
-        logging.debug("content of {} : {}".format(file_path, content))
-        if content == expected_content:
-            print(f"{colors['green']} content of {file} -> OK {colors['reset']}")
-        else:
-            print(f"{colors['red']} content of {file} -> KO {colors['reset']}")
+    if os.path.exists(file_path):
+        print(f"{colors.get('green')} {file_path} -> found {colors['reset']}")
+        with open(file_path, "r") as file:
+            read_content = file.read()
+            logging.debug(
+                "File {}\nExpected content: {}\nRead content: {}".format(
+                    file_path, expected_content, read_content
+                )
+            )
+
+            if compare_two_text(expected_content, read_content):
+                print(
+                    f"{colors['green']} content of {file_path} -> OK {colors['reset']}"
+                )
+            else:
+                print(f"{colors['red']} content of {file_path} -> KO {colors['reset']}")
+    else:
+        print(f"{colors['red']} {file_path} -> not found {colors['reset']}")
 
 
 def check():
@@ -53,22 +83,20 @@ def check():
     folder = "/home/etudiant/exercice1"
     file1 = "exercice1_a.txt"
     file2 = "exercice1-b.txt"
-    expected_content_file1 = "Premier test de création de fichier"
-    expected_content_file2 = "Deuxième test de création de fichier.\nUtilisation d'un éditeur.\nC'est moins rapide mais ça fonctionne aussi."
+    expected_content_file1 = """Premier test de création de fichier\n"""
+    expected_content_file2 = """Deuxième test de création de fichier.
+    Utilisation d'un éditeur.
+    C'est moins rapide mais ça fonctionne aussi."""
     if os.path.exists(folder):
         print(f"{colors['green']} {folder} -> found {colors['reset']}")
-        if file_path := os.path.exists(os.path.join(folder, file1)):
-            print(f"{colors.get('green')} {file_path} -> OK {colors['reset']}")
-            check_content_of_file(
-                file_path=file_path, expected_content=expected_content_file1
-            )
-        if file_path := os.path.exists(os.path.join(folder, file2)):
-            print(f"{colors.get('green')} {file_path} -> OK {colors['reset']}")
-            check_content_of_file(
-                file_path=file_path, expected_content=expected_content_file1
-            )
-        else:
-            print(f"{colors['red']} {file_path} -> not found {colors['reset']}")
+        file_path = os.path.join(folder, file1)
+        check_content_of_file(
+            file_path=file_path, expected_content=expected_content_file1
+        )
+        file_path = os.path.join(folder, file2)
+        check_content_of_file(
+            file_path=file_path, expected_content=expected_content_file1
+        )
     else:
         print(f"{colors['red']} {folder} -> not found {colors['reset']}")
 
