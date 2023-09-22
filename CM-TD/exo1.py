a17f2a239fc4836c1af42bb69792be992f57f838
Florent LORNE <florent.lorne@univ-smb.fr>
2023-09-22 11:23:40 +0200

    Resolve problem of indentation of multiple lines

--- CM-TD/exo1.py
+++ CM-TD/exo1.py
@@ -7,7 +7,7 @@ import subprocess
 import signal
 import shutil
 import difflib
-from pprint import pprint
+from textwrap import dedent
 
 colors = {"red": "\033[31m", "green": "\033[32m", "reset": "\033[0m"}
 
@@ -29,8 +29,7 @@ def delete_history():
 
 
 def init():
-    logging.debug("Init not finished yet. Don't use")
-    delete_history()
+    # delete_history()
     home_directory = os.path.expanduser("~")
     path = os.path.join(home_directory, "exercice1")
     shutil.rmtree(path)
@@ -42,18 +41,32 @@ def init():
 def compare_two_text(text1, text2):
     """
     Give two text and compare them
+    Return : {"same_text":same_text,"diff":diff}
+    same_text : Boolean
+    diff : differences between text
     """
     text1 = text1.splitlines(keepends=False)
     text2 = text2.splitlines(keepends=False)
     differ = difflib.Differ()
-    diff = list(differ.compare(text1, text2))
+    diff = differ.compare(text1, text2)
+    diff_list = list(diff)
+    logging.debug("Diff_list : {}".format(diff_list))
     same_text = True
-    for line in diff:
-        if line[0] != " ":
+    for i, line in enumerate(diff_list):
+        if line[0:1] != " ":
+            try:
+                text1_line = text1[i]
+            except IndexError:
+                text1_line = ""
+            try:
+                text2_line = text2[i]
+            except IndexError:
+                text2_line = ""
+            diff = f"Line {i} are not the same : {text1_line}  {text2_line}"
             same_text = False
-            pprint(diff)
+
             break
-    return same_text
+    return {"same_text": same_text, "diff": diff}
 
 
 def check_content_of_file(file_path, expected_content):
@@ -66,43 +79,49 @@ def check_content_of_file(file_path, expected_content):
                     file_path, expected_content, read_content
                 )
             )
-
-            if compare_two_text(expected_content, read_content):
+            comparison = compare_two_text(expected_content, read_content)
+            if comparison["same_text"]:
                 print(
                     f"{colors['green']} content of {file_path} -> OK {colors['reset']}"
                 )
             else:
                 print(f"{colors['red']} content of {file_path} -> KO {colors['reset']}")
+                print(
+                    f"Diff between expected and read content of {file_path} :\n{comparison.get('diff', '')}"
+                )
+
     else:
         print(f"{colors['red']} {file_path} -> not found {colors['reset']}")
 
 
 def check():
-    logging.debug("Check not finished yet. Don't use")
     # Check exercice1_a.txt
     folder = "/home/etudiant/exercice1"
     file1 = "exercice1_a.txt"
     file2 = "exercice1-b.txt"
     expected_content_file1 = """Premier test de création de fichier\n"""
-    expected_content_file2 = """Deuxième test de création de fichier.
-    Utilisation d'un éditeur.
-    C'est moins rapide mais ça fonctionne aussi."""
+    expected_content_file2 = """Deuxième test de création de fichier.\nUtilisation d'un éditeur.\nC'est moins rapide mais ça fonctionne aussi."""
     if os.path.exists(folder):
         print(f"{colors['green']} {folder} -> found {colors['reset']}")
         file_path = os.path.join(folder, file1)
+        # Test file1 content
         check_content_of_file(
             file_path=file_path, expected_content=expected_content_file1
         )
         file_path = os.path.join(folder, file2)
+        # Test file2 content
         check_content_of_file(
-            file_path=file_path, expected_content=expected_content_file1
+            file_path=file_path, expected_content=expected_content_file2
         )
     else:
         print(f"{colors['red']} {folder} -> not found {colors['reset']}")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
+    logging.info(
+        "{}PROGRAM not finished yet. Don't use{}".format(colors["red"], colors["reset"])
+    )
     parser = argparse.ArgumentParser(
         prog="exo1",
         description="Programme de test et d'initialisation de l'exo1 lié au CM et TD",
