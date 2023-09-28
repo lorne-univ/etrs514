4269846c9256189b291e7e18e21598699d1f4ac6
Florent LORNE <florent.lorne@univ-smb.fr>
2023-09-28 09:49:21 +0200

    missing : in check_permissions

--- CM-TD/exercice2.py
+++ CM-TD/exercice2.py
@@ -212,7 +212,7 @@ def check_permissions(file, expected_permission):
         if group == expected_permission["group"]:
             # We focus only on the ISUID/ISGID/ISVTX and owner and group and other permissions [-4:]
             mode = oct(statinfo.st_mode & 0o777)[-4:]
-            if check_mode(mode,expected_permission['mode'])
+            if check_mode(mode, expected_permission["mode"]):
                 print_green(f"{file} permissons are correct")
                 return True
             else:
@@ -242,11 +242,11 @@ def check_step2():
     if os.path.exists(folder):
         print_green(f"{folder} exists")
         # Check permission
-        check_permissions(folder,{'owner':'root','group':'root','mode':'07!7'})
+        check_permissions(folder, {"owner": "root", "group": "root", "mode": "07!7"})
 
     else:
         print_red(f"{folder} not found")
-    file1 = "exercic"
+    file1 = "exercic1"
 
 
 def check_step3():
